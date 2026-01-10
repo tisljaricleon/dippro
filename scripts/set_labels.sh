a905d52f482e44009152e6f11fad888b3f326735
ForsakenGentleman <50748237+ForsakenGentleman@users.noreply.github.com>
2026-01-10 16:06:08 +0100

    abc

--- scripts/set_labels.sh
+++ scripts/set_labels.sh
@@ -1,14 +1,9 @@
 # global aggregator
-kubectl label --overwrite nodes ga fl/type=global_aggregator
-
-# local aggregator
-kubectl label --overwrite nodes la-1 fl/type=local_aggregator
-kubectl label --overwrite nodes la-1 comm/ga=100
+kubectl label --overwrite nodes user fl/type=global_aggregator
 
 # client
-kubectl label --overwrite nodes cl-1 fl/type=client
-kubectl label --overwrite nodes cl-1 comm/ga=120
-kubectl label --overwrite nodes cl-1 comm/la-1=20
-kubectl label --overwrite nodes cl-1 comm/la-2=60
-kubectl label --overwrite nodes cl-1 fl/num-partitions=10
-kubectl label --overwrite nodes cl-1 fl/partition-id=0
+kubectl label --overwrite nodes sem2-desktop fl/type=client
+kubectl label --overwrite nodes sem2-desktop comm/user=100
+kubectl label --overwrite nodes sem2-desktop fl/num-partitions=2
+kubectl label --overwrite nodes sem2-desktop fl/partition-id=0
+
