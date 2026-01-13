ff4456c20c50a81819b141a23f29d9c3ac33dc73
ForsakenGentleman <50748237+ForsakenGentleman@users.noreply.github.com>
2026-01-14 00:22:28 +0100

    ABCD

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,6 +1,6 @@
 cd ~/Desktop/dippro
 sudo bash scripts/cleanup.sh default
 cd ~/Desktop
-sudo rm -rf dippro
+rm -rf dippro
 git clone https://github.com/tisljaricleon/dippro.git
-sudo cp /etc/rancher/k3s/k3s.yaml ~/Desktop/dippro/configs/cluster/kube_config.yaml
+cp /etc/rancher/k3s/k3s.yaml ~/Desktop/dippro/configs/cluster/kube_config.yaml
