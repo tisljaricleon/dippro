a6216ac9236f270e57bcb4224615ce45ac2c22cc
ForsakenGentleman <50748237+ForsakenGentleman@users.noreply.github.com>
2026-01-17 21:21:58 +0100

    yesir

--- scripts/set_labels.sh
+++ scripts/set_labels.sh
@@ -1,14 +1,14 @@
 # global aggregator
-kubectl label --overwrite nodes user fl/type=global_aggregator
+kubectl label --overwrite nodes raspberrypi fl/type=global_aggregator
 
 # Jetson (ARM64) client node
 kubectl label --overwrite nodes sem2-desktop fl/type=client kubernetes.io/arch=arm64
-kubectl label --overwrite nodes sem2-desktop comm/user=100
+kubectl label --overwrite nodes sem2-desktop comm/raspberrypi=100
 kubectl label --overwrite nodes sem2-desktop fl/num-partitions=2
 kubectl label --overwrite nodes sem2-desktop fl/partition-id=0
 
 # Normal (x86_64) client node
 kubectl label --overwrite nodes worker fl/type=client kubernetes.io/arch=amd64
-kubectl label --overwrite nodes worker comm/user=100
+kubectl label --overwrite nodes worker comm/raspberrypi=100
 kubectl label --overwrite nodes worker fl/num-partitions=2
 kubectl label --overwrite nodes worker fl/partition-id=1
